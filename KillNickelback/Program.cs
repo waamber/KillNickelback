55d8a3bc69d45147aae385d2551c591a076a56e3
waamber <amberastuart@aol.com>
2018-01-23 11:07:54 -0600

    added song lists

--- KillNickelback/Program.cs
+++ KillNickelback/Program.cs
@@ -10,6 +10,32 @@ namespace KillNickelback
     {
         static void Main(string[] args)
         {
+            var goodSongs = new List<Song>();
+            var allSongs = new HashSet<Song>();
+
+            allSongs.Add(new Song { Artist = "Nickleback", Name = "Photograph" });
+            allSongs.Add(new Song { Artist = "NSYNC", Name = "Merry Christmas" });
+            allSongs.Add(new Song { Artist = "Nickleback", Name = "How You Remind Me" });
+            allSongs.Add(new Song { Artist = "Nickleback", Name = "Rockstar" });
+            allSongs.Add(new Song { Artist = "Mariah Carey", Name = "All I Want For Christmas Is You" });
+            allSongs.Add(new Song { Artist = "Frank Sinatra", Name= "Jingle Bells" });
+            allSongs.Add(new Song { Artist = "The Temptations", Name = "Silent Night" });
+
+            foreach (var song in allSongs)
+            {
+                if( song.Artist != "Nickleback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Name} by {song.Artist}.");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
